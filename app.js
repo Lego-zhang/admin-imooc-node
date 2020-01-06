8626794d530a3fbd2f249d3b3a22cb8edb7b43b1
Lego-zhang <1945465017@qq.com>
2020-01-06 15:24:48 +0800

    登录路由创建/解析请求body

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require("express");
+const router = require("./router");
+
+const fs = require("fs");
+const https = require("https");
+
+// 解析 boby
+const bodyParser = require("body-parser");
 
 // 创建 express 应用
 const app = express();
 
-const router = require("./router");
+// 解析路由参数
+app.use(bodyParser.urlencoded({ extended: true }));
+// 解析json 形式的body
 
 app.use("/", router);
 // 监听 / 路径的 get 请求
+app.use(bodyParser.json());
+
+const privateKey = fs.readFileSync("https/summerxiatian.top.key", "utf8");
+const certificate = fs.readFileSync("https/summerxiatian.top.pem", "utf8");
+
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
+
+const SSLPORT = 18082;
 
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {
   const { address, port } = server.address();
   console.log("Http Server is running on http://%s:%s", address, port);
+});
+
+httpsServer.listen(SSLPORT, function() {
+  console.log("HTTPS Server is running on: https://localhost:%s", SSLPORT);
 });
