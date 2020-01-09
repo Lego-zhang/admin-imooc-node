f91dab8f835acda5abdd6724df57df24c5d8d14f
Lego-zhang <1945465017@qq.com>
2020-01-09 18:35:41 +0800

    express-validator 验证器的使用

--- router/user.js
+++ router/user.js
@@ -5,25 +5,50 @@ const Result = require("../models/Result");
 const { md5 } = require("../utils/index");
 
 const { login } = require("../services/user");
+
 const { PWD_SALT } = require("../utils/constant");
+
+// 表单校验 body方法
+const { body, validationResult } = require("express-validator");
+
+const boom = require("boom");
 
 const router = express.Router();
 
-router.post("/login", function(req, res) {
-  // console.log(req.body);
-  let { username, password } = req.body;
+router.post(
+  "/login",
+  [
+    body("username")
+      .isString()
+      .withMessage("用户名必须为字符"),
+    body("password")
+      .isNumeric()
+      .withMessage("密码必须为数字")
+  ],
+  function(req, res, next) {
+    // 使用验证 validationResult()
+    const err = validationResult(req);
+    console.log(err);
 
-  password = md5(`${password}${PWD_SALT}`);
-
-  login(username, password).then(user => {
-    console.log(user + "1");
-    if (!user || user.length === 0) {
-      new Result("登录失败").fail(res);
+    if (!err.isEmpty()) {
+      // const msg  = err.errors[0].msg
+      const [{ msg }] = err.errors;
+      next(boom.badRequest(msg));
     } else {
-      new Result("登录成功").success(res);
+      let { username, password } = req.body;
+
+      password = md5(`${password}${PWD_SALT}`);
+
+      login(username, password).then(user => {
+        if (!user || user.length === 0) {
+          new Result("登录失败").fail(res);
+        } else {
+          new Result("登录成功").success(res);
+        }
+      });
     }
-  });
-});
+  }
+);
 
 router.get("/info", function(req, res, next) {
   res.json("user info...");
