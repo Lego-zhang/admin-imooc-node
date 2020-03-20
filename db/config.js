e2c283d3262262c483cd6a2140f8d5c90fcc577a
Lego-zhang <1945465017@qq.com>
2020-03-20 15:45:38 +0800

    更改为生产环境

--- db/config.js
+++ db/config.js
@@ -1,5 +1,8 @@
+// const { env } = require("../utils/env");
+// const dbHost = env === "dev" ? "localhost" : "47.92.75.132";
+
 module.exports = {
-  host: "localhost",
+  host: "47.92.75.132",
   user: "root",
   password: "Mysql-123456",
   database: "book"
